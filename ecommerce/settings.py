22c1e00eb726adb1636bf577c02f4c996a46de95
unknown <devlexlong@gmail.com>
2022-02-06 10:35:24 -0500

    corrected allowed host url

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'zhv%265(q5u4f1k%l*pf2he!my@j=e6(652hg-o9*+yz+cco*e'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['djangostore-lexlong.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = [ '127.0.0.1', 'djangostore-lexlong.herokuapp.com',]
 
 
 # Application definition
